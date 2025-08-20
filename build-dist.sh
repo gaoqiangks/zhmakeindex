cce987cb88920a7c92b92d2d830b71c3108bc166
gaoqiang <gaoqiangks@qq.com>
2025-08-21 03:20:20 +0800

    aa

--- build-dist.sh
+++ build-dist.sh
@@ -9,27 +9,17 @@ else
   zhmVersion="devel"
 fi
 
-zhmRevision=$(git log -1 --pretty=format:"%h(%ai")
-zhmRevision=${zhmRevision:0:18}
-FLAGS="-ldflags -X main.Version=$zhmVersion -X main.Revision=$zhmRevision"
+zhmRevision=$(git log -1 --pretty=format:"%h(%ai)")
+zhmRevision="${zhmRevision:0:18})"
 
-GOOS=windows
-GOARCH=386
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/windows_x86/zhmakeindex.exe
-GOARCH=amd64
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/windows_x64/zhmakeindex.exe
-
-GOOS=linux
-GOARCH=386
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/linux_x86/zhmakeindex
-GOARCH=amd64
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/linux_x64/zhmakeindex
+FLAGS="-ldflags \"-X main.Version=$zhmVersion -X main.Revision=$zhmRevision\""
+#
+# echo $FLAGS
+# exit
 
 GOOS=darwin
-GOARCH=386
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/darwin_x86/zhmakeindex
-GOARCH=amd64
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/darwin_x64/zhmakeindex
 
 GOARCH=arm64
-GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/darwin_arm64/zhmakeindex
+# GOOS=$GOOS GOARCH=$GOARCH go build $FLAGS -o bin/darwin_arm64/zhmakeindex
+cmd=$(echo "go build $FLAGS -o bin/darwin_arm64/zhmakeindex")
+eval $cmd
--- build-macos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-GOOS=darwin GOARCH=arm64 go build $FLAGS -o bin/darwin_arm64/zhmakeindex
